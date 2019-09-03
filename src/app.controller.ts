9da0478091b30afabd5087659fb3f57382fa982d
zhengqbbb <1074059947@qq.com>
2019-09-04 01:15:57 +0800

    :tada: first commit nestjs

---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from '@nestjs/common';
+//import { AppService } from './app.service';
+
+//代表一个控制器
+//使用依赖注入 appService 把AppService注入进来
+@Controller()
+export class AppController {
+  //constructor(private readonly appService: AppService) {}
+
+  //表明这是一个Get请求方法，：string表达返回值是一个string
+  @Get()
+  /*  getHello(): string {
+     return this.appService.getHello();
+   } */
+  index() {
+    return 'index'
+  }
+}
---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+/**
+ * @是一个装饰器，可以理解为下面的class可以是个类可以是方法可以是个参数也可以是是变量，对他进行一个装饰 
+ * @Module 代表下面这个是个模块
+ */
+@Module({
+  //可以导入某些模块
+  imports: [],
+  //相当于express下的router路由
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
