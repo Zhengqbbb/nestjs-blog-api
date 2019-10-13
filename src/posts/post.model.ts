e0c59be08a0e56f986bc9e5b281c77ea0c5d3a26
zhengqbbb <1074059947@qq.com>
2019-10-13 19:59:15 +0800

    :heavy_plus_sign: Add nest-curd

--- src/posts/post.model.ts
+++ src/posts/post.model.ts
@@ -1,8 +1,11 @@
 import { prop } from '@typegoose/typegoose'
+import { ApiModelProperty } from '@nestjs/swagger';
 
 export class Post {
+  @ApiModelProperty({ description: '帖子标题', example: '帖子标题default' })
   @prop()
   title: string
+  @ApiModelProperty({ description: '帖子文章', example: '帖子文章default' })
   @prop()
   content: string
 }
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -5,6 +5,7 @@ import { InjectModel } from 'nestjs-typegoose';
 import {Post as PostSchema} from './post.model';
 import { ModelType } from '@typegoose/typegoose/lib/types';
 
+import { Crud } from 'nestjs-mongoose-crud';
 class CreatePostDto {
   @ApiModelProperty({ description: '帖子标题', example: '帖子标题default' })
   @IsNotEmpty({message: '请填写标题'})
@@ -14,53 +15,24 @@ class CreatePostDto {
 }
 
 //路由前缀
+@Crud({
+  model: PostSchema,
+  routes: {
+    find: {
+      decorators: [
+        ApiOperation({title: '帖子列表'})
+      ]
+    },
+    create: {
+      dto: CreatePostDto
+    }
+  }
+})
 @Controller('posts')
 @ApiUseTags('帖子')
 export class PostsController {
   //使用依赖注入Post模型
   constructor(
-    @InjectModel(PostSchema) private readonly postModel: ModelType<PostSchema>
+    @InjectModel(PostSchema) private readonly model: ModelType<PostSchema>
   ){ }
-
-  @Get()
-  //给API区分
-  @ApiOperation({ title: '帖子列表' })
-  async index() {
-    return await this.postModel.find();
-  }
-
-  @Post()
-  @ApiOperation({ title: '创建帖子' })
-  //使用参数装饰器获取请求体数据,从get中获取参数
-  //create(@Body() body,@Query() query, @Param() params){
-  async create(@Body() createPostDto: CreatePostDto) {
-    await this.postModel.create(createPostDto);
-    return {
-      success: true
-    }
-  }
-
-  @Get(':id')
-  @ApiOperation({ title: '帖子详情' })
-  async detail(@Param('id') id: string) {
-    return await this.postModel.findById(id);
-  }
-
-  @Put(':id')
-  @ApiOperation({ title: '编辑帖子' })
-  async updata(@Param('id') id: string, @Body() updataPostDto: CreatePostDto) {
-    await this.postModel.findByIdAndUpdate(id, updataPostDto);
-    return {
-      success: true
-    }
-  }
-
-  @Delete(':id')
-  @ApiOperation({ title: '删除帖子' })
-  async remove(@Param('id') id: string) {
-    await this.postModel.findByIdAndDelete(id);
-    return {
-      success: true
-    }
-  }
 }
